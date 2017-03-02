d49028bc49048f9c17b7d028b145dc1810404fee
Timothy Wang <wangtim1996@gmail.com>
2017-03-02 10:09:09 -0800

    lab 8 done

--- public/js/camera.js
+++ public/js/camera.js
@@ -1,8 +1,8 @@
 function initCamera() {
   //Uncomment and fill in the correct selectors below.
-  //capture($('<Your code here>'),
-  //        $('<Your code here>'),
-  //        $('<Your code here>'));
+  capture($('#camera-video'),
+          $('#camera-canvas'),
+          $('#camera-button'));
 }
 
 function capture(video, canvas, snapshotButton) {
@@ -38,4 +38,3 @@ function capture(video, canvas, snapshotButton) {
       successCallback, errorCallback);
 
 };
-
