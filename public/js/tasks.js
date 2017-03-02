be055a5c75a78426c7ef031245d69ade6cac4788
Timothy Wang <wangtim1996@gmail.com>
2017-03-02 10:35:35 -0800

    tasks

--- public/js/tasks.js
+++ public/js/tasks.js
@@ -1,1 +1,21 @@
 // Your code goes here
+$(document).ready(function() {
+  console.log("LOADING");
+
+  $('#taskForm').submit(function(e) {
+    // Prevents default submit + reload (we only want the submit part)
+    console.log("TESTING");
+    e.preventDefault();
+
+    var taskName = $('#addTask').val();
+
+    // Send the POST request
+    $.post('addTask', { taskName: taskName }, postCallback);
+  });
+});
+
+function postCallback(res) {
+  $('.taskList').append('<li>' + res + '</li>'); // Add to the list
+
+ $('#addTask').val(''); // Clear form
+}
